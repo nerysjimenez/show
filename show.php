6f474bcdc253f94df7201a5a602cf0086c099fcd
Nerys Jimenez <jimenezpichardon785@strose.edu>
2017-11-05 16:09:53 -0500

    Add files via upload

---
+++ show.php
@@ -0,0 +1,32 @@
+<?php
+header("Cache-Control: post-check=1,pre-check=2");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Content-type: text/xml");
+echo "<?xml version=\"1.0\"?>";
+echo "<root>";  
+
+$id = $_GET["i"];
+
+$DBConnect = mysql_connect("localhost", "jimenez", "");
+mysql_select_db("jimenez");
+
+$query ="SELECT NAME, STATE, YEAR_ELECTED, PARTY " .
+        "FROM PRESIDENTS " .
+        "WHERE PRESIDENT_ID = ${id}";
+
+$QueryResult = mysql_query($query);
+$Row = mysql_fetch_row($QueryResult);
+
+if($Row)
+{
+  echo "<President>";
+  echo "<Name>" . $Row[0] . "</Name>";
+  echo "<State>" . $Row[1] . "</State>";
+  echo "<Year>" . $Row[2] . "</Year>";
+  echo "<Party>" . $Row[3] . "</Party>";
+  echo "</President>";
+}
+echo "</root>";
+
+?>
